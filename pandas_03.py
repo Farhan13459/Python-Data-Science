d3c0811416ae5c59a103d509de6243ccb054a7c9
Farhan13459 <giki5566@gmail.com>
2025-01-13 21:04:52 -0800

    Remove duplication in Customers csv file

--- pandas_03.py
+++ pandas_03.py
@@ -2,13 +2,10 @@ import pandas as pd
 import numpy as np
 
 cust=pd.read_csv("Customers.csv")
-
-print(cust.drop_duplicates(subset=["Age" , "Annual Income ($)","Spending Score (1-100)"],keep="last").tail(10))
-
-print(cust.drop_duplicates(subset=["Profession","Family Size"]).head(20))
-
-print(cust.drop_duplicates(subset=["Profession"]).head(20))
-
+print(cust.size)
+cust.drop_duplicates(subset=["Age" , "Annual Income ($)","Spending Score (1-100)"],keep="last", inplace=True)
+cust.to_csv("Customers.csv")
+print(cust.size)
 
 
 # cust.drop_duplicates().head(20)
